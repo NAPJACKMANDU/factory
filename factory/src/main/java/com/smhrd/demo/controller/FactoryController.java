39e9c522642b3cddf5e67633ecf09363225a7228
wnsgud6707 <wnsgud6707@icloud.com>
2024-11-26 10:49:46 +0900

    Add backend

---
+++ factory/src/main/java/com/smhrd/demo/controller/FactoryController.java
@@ -0,0 +1,23 @@
+package com.smhrd.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FactoryController {
+	
+	@GetMapping("/")
+	public String mainPage()
+	{
+		return "P01_Main";
+	}
+	@GetMapping("/monitor")
+	public String goMonitor() {
+		return "P04_Monitor";
+	}
+	@GetMapping("/login")
+	public String Login() {
+		return "P02_LogIn";
+	}
+	
+}
