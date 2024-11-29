aa40e9083b6fd9d542dc4d36cf294ca30db89ace
sjw07028 <sjw07028@naver.com>
2024-11-30 03:01:02 +0900

    전부 수정

--- factory/src/main/java/com/smhrd/demo/model/FactoryMember.java
+++ factory/src/main/java/com/smhrd/demo/model/FactoryMember.java
@@ -1,13 +1,9 @@
 package com.smhrd.demo.model;
 
 import java.sql.Timestamp;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -16,7 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -25,6 +21,54 @@ import lombok.ToString;
 public class FactoryMember {
 
     
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+
+	public int getCompanyIdx() {
+		return companyIdx;
+	}
+
+	public void setCompanyIdx(int companyIdx) {
+		this.companyIdx = companyIdx;
+	}
+
 	@Id
 	@Column(nullable = false, updatable = false)
 	private String id; // 회원 아이디 (고유 식별자)
@@ -42,6 +86,9 @@ public class FactoryMember {
     private Timestamp created_at; // 생성 일자
 
     private int companyIdx; // 회사 식별자
+
+
+
 
 	
 }
