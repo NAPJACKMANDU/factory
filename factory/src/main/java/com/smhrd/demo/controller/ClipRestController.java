63af7f260aa34e8b102f9b7fb4d008f699875662
sjw07028 <sjw07028@naver.com>
2024-12-05 17:54:33 +0900

    카메라인덱스 추가

--- factory/src/main/java/com/smhrd/demo/controller/ClipRestController.java
+++ factory/src/main/java/com/smhrd/demo/controller/ClipRestController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smhrd.demo.model.ClipModel;
 import com.smhrd.demo.service.ClipService;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class ClipRestController {
@@ -19,10 +22,12 @@ public class ClipRestController {
 	
 	  // 파일 업로드 처리 메서드
     @PostMapping("/videos/upload")
-    public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile file,  @RequestParam("cameraIndex") int cameraIndex , HttpSession session) {
         try {
         	System.out.println("여기에 오나요?");
-        	service.saveVideo(file); // 동영상 저장 서비스 호출
+        	session.setAttribute("cameraIndex", cameraIndex) ;
+        	service.saveVideo(file, cameraIndex); // 동영상 저장 서비스 호출
+        	
             return ResponseEntity.ok("동영상이 성공적으로 업로드되었습니다."); // 성공 메시지 반환
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
